67e27e11e4dc00928ef4395db8b47c09f32bf5af
xthian82 <crecalde@gmail.com>
2020-04-23 00:11:29 -0400

    initial pages

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,28 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './shared/header/header.component';
+import { FooterComponent } from './shared/footer/footer.component';
+import { ShoppingCartComponent } from './shopping-cart/shopping-cart.component';
+import { NavComponent } from './shared/nav/nav.component';
+import { ProductListComponent } from './shopping-cart/product-list/product-list.component';
+import { FiltersComponent } from './shopping-cart/filters/filters.component';
+import { CartComponent } from './shopping-cart/cart/cart.component';
+import { CartItemComponent } from './shopping-cart/cart-item/cart-item.component';
+import { ProductItemComponent } from './shopping-cart/product-list/product-item/product-item.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    ShoppingCartComponent,
+    NavComponent,
+    ProductListComponent,
+    FiltersComponent,
+    CartComponent,
+    CartItemComponent,
+    ProductItemComponent
   ],
   imports: [
     BrowserModule
